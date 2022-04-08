d67eb44ef5e3919de4f8dd21e3f4585374cb0665
李聪 <licong@qianxin.com>
2022-04-08 13:39:52 +0800

    fix ci

--- util_test.go
+++ util_test.go
@@ -1,16 +1,20 @@
 package main
 
 import (
+	"runtime"
 	"testing"
 )
 
 func TestRemoveChars(t *testing.T) {
-	filename := `\Wha's Like Us? (Say It in Scots!).epub`
-	if IsValidFilename(filename) {
-		t.Fail()
+	if runtime.GOOS == "windows" {
+		filename := `\Wha's Like Us? (Say It in Scots!).epub`
+		if IsValidFilename(filename) {
+			t.Fail()
+		}
+		newName := removeChars(filename, IllegalCharacters)
+		if !IsValidFilename(newName) {
+			t.Fail()
+		}
 	}
-	newName := removeChars(filename, IllegalCharacters)
-	if !IsValidFilename(newName) {
-		t.Fail()
-	}
+
 }
